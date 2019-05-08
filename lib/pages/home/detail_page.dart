15839bb5f98cac64de60deb69c6aae8934f60970
渺点 <rocklobster.ldk@alibaba-inc.com>
2019-05-08 19:35:59 +0800

    增加ListView 增加Router

---
+++ lib/pages/home/detail_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DetailPage extends StatelessWidget {
+  final Todo todo;
+    DetailPage({Key key,@required this.todo}) : super(key:key);
+  
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("${todo.title}"),
+        ),
+        body: new Center(
+          child: new GestureDetector(
+            child: new Text("${todo.description}"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      );
+    }
+  }
+  
+  class Todo {
+    final String title;
+    final String description;
+    Todo(this.title,this.description);
+}
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import '../home/detail_page.dart';
+
+final todos = new List<Todo>.generate(
+    20,
+        (i) => new Todo(
+        "Todo $i", "A description of what needs to be done for Todo $i"));
 
 class HomePage extends StatefulWidget {
 
@@ -10,6 +16,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> list = List.generate(10, (index) => "This is number $index");
+
 @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +26,41 @@ class _HomePageState extends State<HomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      body: Center(
+        child: getBody(),
+      ),
+    );
+  }
+  getBody() {
+    return ListView.builder(
+      itemCount: list.length, 
+      itemBuilder: (BuildContext context, int index) {
+        return getItem(index,list[index]);
+      });
+  }
+  getItem(int index, String title) {
+    //return new Text(title);
+    return new Card(
+      child: new FlatButton(
+        onPressed: () => _routerVC(index),
+        child: new ListTile(
+          title: new Text(index.toString()),
+          subtitle: new Text(title),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+        ),
+      ),
+    );
+  }
+  _routerVC(int index) {
+    print('点击:${index.toString()}');
+    print(todos[index]);
+    Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (context) => 
+        new DetailPage(todo: todos[index],)
+      )
     );
   }
 }
