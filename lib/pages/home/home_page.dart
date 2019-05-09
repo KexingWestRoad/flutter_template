26914f856647109f45b18665af7ed51f2262fd63
渺点 <rocklobster.ldk@alibaba-inc.com>
2019-05-09 11:46:45 +0800

    新增ListView上拉下拉刷新

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import '../home/detail_page.dart';
 
-final todos = new List<Todo>.generate(
-    20,
-        (i) => new Todo(
-        "Todo $i", "A description of what needs to be done for Todo $i"));
-
 class HomePage extends StatefulWidget {
 
   HomePage({Key key, this.title}) : super(key: key);
@@ -16,7 +11,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> list = List.generate(10, (index) => "This is number $index");
+  List<String> list;
+  List<Todo> todos;
+  ScrollController _controller;
+
+@override
+void initState() {
+  super.initState();
+  list = List.generate(10, (index) => "This is number $index");
+  todos = List<Todo>.generate(list.length, (index) => new Todo("Todo $index", "A description of what needs to be done for Todo $index"));
+  _controller = ScrollController();
+  _controller.addListener(() {
+    if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+        _getMoreData();
+    }
+  });
+}
+
+@override
+void dispose() {
+  _controller.dispose();
+  super.dispose();
+}
 
 @override
   Widget build(BuildContext context) {
@@ -31,14 +47,25 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  getBody() {
-    return ListView.builder(
-      itemCount: list.length, 
-      itemBuilder: (BuildContext context, int index) {
-        return getItem(index,list[index]);
-      });
+  Widget getBody() {
+    return new RefreshIndicator(
+      onRefresh: _refresh,
+      color: Colors.blue,
+      child: ListView.builder(
+        controller: _controller,
+        itemCount: list.length + 1, 
+        itemBuilder: (BuildContext context, int index) {
+          if (index == list.length) {
+            print("展示上拉加载更多进度");
+            return loadMoreProgressIndicator();
+          } else {
+            return getItem(index,list[index]);
+          }
+        },
+      ),
+    );
   }
-  getItem(int index, String title) {
+  Widget getItem(int index, String title) {
     //return new Text(title);
     return new Card(
       child: new FlatButton(
@@ -53,14 +80,46 @@ class _HomePageState extends State<HomePage> {
     );
   }
   _routerVC(int index) {
-    print('点击:${index.toString()}');
-    print(todos[index]);
+    
     Navigator.push(
       context,
       new MaterialPageRoute(
         builder: (context) => 
         new DetailPage(todo: todos[index],)
       )
+    );
+  }
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        list.clear();
+        list = List.generate(10, (index) => "This is number $index");
+        todos = List<Todo>.generate(list.length, (index) => new Todo("Todo $index", "A description of what needs to be done for Todo $index"));
+        return;
+      });
+    });
+  }
+  Future _getMoreData() async {
+    print("获取更多数据");
+    await Future.delayed(Duration(seconds: 2),() {
+      setState(() {
+        List<String> moreData = List.generate(5, (index) => "add data $index");
+        list.addAll(moreData);
+        todos = List<Todo>.generate(list.length, (index) => new Todo("Todo $index", "A description of what needs to be done for Todo $index"));
+      });
+    });
+  }
+  Widget loadMoreProgressIndicator() {
+    return new Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: new Center(
+        child: new Opacity(
+          opacity: 1.0,
+          child: new CircularProgressIndicator(
+            strokeWidth: 2.0,
+          ),
+        ),
+      ),
     );
   }
 }
